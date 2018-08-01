cf1683869b8235190316aebb5a762c559a8c51b0
Laci556 <bucsailaca@gmail.com>
2018-08-01 17:33:35 +0200

    gomb es dinamikus canvas

--- sketch.js
+++ sketch.js
@@ -1,10 +1,45 @@
+let gomb;
+
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(windowWidth, windowHeight);
+  gomb = new Gomb(0, 0, 50, "hajitas/asd.html");
 }
 
 function draw() {
-  background(0);
-  noStroke();
-  fill(255);
-  ellipse(0, 0, 50):
+  background(51);
+  gomb.show();
+}
+
+class Gomb {
+  constructor(x, y, r, url, felirat) {
+    this.x = x;
+    this.y = y;
+    this.url = url;
+    this.r = r;
+    this.felirat = felirat;
+  }
+
+  show() {
+    fill(255);
+    noStroke();
+    ellipse(width/2 + this.x, height/2 + this.y, this.r*2);
+    textSize(28);
+    fill(100);
+    textAlign(CENTER, CENTER)
+    text(str,x,y,x2,y2)
+  }
+
+  mouseClicked() {
+    if (dist(width/2 + this.x, height/2 + this.y, mouseX, mouseY) < this.r) {
+      window.location.href = this.url;
+    }
+  }
+}
+
+function mouseClicked() {
+  gomb.mouseClicked();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
