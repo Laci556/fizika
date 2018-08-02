c1ad816181fd200fa9f4cc9db89a242d7883e76b
Laci556 <bucsailaca@gmail.com>
2018-08-02 16:47:08 +0200

    vegso design

--- sketch.js
+++ sketch.js
@@ -1,12 +1,37 @@
 let gomb;
+let monsterratBoldItalic;
+let monsterratSemiBoldItalic
+
+function preload() {
+  monsterratBoldItalic = loadFont('fonts/Montserrat-ExtraBoldItalic.ttf');
+  monsterratSemiBoldItalic = loadFont('fonts/OpenSans-SemiBoldItalic.ttf');
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  gomb = new Gomb(0, 0, 50, "hajitas/asd.html", 'Hajítás');
+  gomb = new Gomb(0, 0, 75, "hajitas/asd.html", 'Hajítás');
+  background(20);
+  for(let i = 0; i < 200; i++) {
+    fill(240);
+    stroke(240);
+    strokeWeight(2);
+    point(random(width), random(height));
+  }
 }
 
 function draw() {
-  background(51);
+  // background(28);
+  // for(let i = 0; i < 100; i++) {
+  //   fill(240);
+  //   stroke(240);
+  //   point(random(width), random(height));
+  // }
+  fill(230, 83, 79);
+  textAlign(CENTER);
+  textSize(180);
+  stroke(230, 83, 79);
+  textFont(monsterratBoldItalic);
+  text('Projektjeim', width/2, 200);
   gomb.show();
 }
 
@@ -20,14 +45,26 @@ class Gomb {
   }
 
   show() {
-    fill(255);
+    push()
+    noStroke();
+    if (dist(width/2 + this.x, height/2 + this.y, mouseX, mouseY) < this.r) {
+      fill(192);
+    } else {
+      fill(226);
+    }
     noStroke();
     ellipse(width/2 + this.x, height/2 + this.y, this.r*2);
-    textSize(28);
-    fill(100);
+    textSize(38);
+    textFont(monsterratSemiBoldItalic);
+    if (dist(width/2 + this.x, height/2 + this.y, mouseX, mouseY) < this.r) {
+      fill(94, 152, 117);
+    } else {
+      fill(94, 152, 117);
+    }
     textAlign(CENTER, CENTER);
     textStyle(ITALIC);
-    text(this.felirat ,width/2 + this.x, height/2 + this.y);
+    text(this.felirat, width/2 + this.x, height/2 + this.y - 7);
+    pop();
   }
 
   mouseClicked() {
